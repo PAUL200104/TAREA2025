03d35b175f8c8d0aa74d4b1953b771d865fdac6b
paul alva <keny.alva.rosales@isten.edu.pe>
2025-06-30 09:48:03 -0500

    TAREA 02

--- Ejercicio02.py
+++ Ejercicio02.py
@@ -1,4 +1,4 @@
-numero = int(input("Ingresa un número N para la cuenta regresiva:"))
+numero = int(input("Ingresa un número N para la cuenta regresiva: "))
 for i in range(numero, -1, -1):
     print(i)
 print("¡Despegue!")
